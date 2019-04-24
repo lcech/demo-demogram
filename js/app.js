94b32e34ffeefc23d5991bf0808470f0bd134824
Lukáš Čech <ja@lukascech.cz>
2019-04-24 14:44:15 +0200

    iframe API

--- js/app.js
+++ js/app.js
@@ -322,6 +322,16 @@ window.btoa = window.btoa || function () {
 })(jQuery);
 
 /*
+ * Init Youtube Iframe API
+ */
+(function() {
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+})();
+
+/*
  * Global Variable for available Youtube players
  */
 var youtubePlayers = [],
